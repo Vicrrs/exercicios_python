56dc19f49a58ff102a7900d3aaa00f785d0dde07
Vicrrs <enricofermi2019@gmail.com>
2023-04-27 13:46:13 -0400

    ex01

--- exceções/ex01.py
+++ exceções/ex01.py
@@ -1,6 +1,7 @@
-# Sintaxe básica
 try:
-    # bloco de código a ser executado
-except:
-    # bloco de código a ser executado caso ocorra uma exceção
-
+    x = int(input("Digite um número: "))
+    y = 1 / x
+except (ValueError, ZeroDivisionError) as e:
+    print("Ocorreu um erro:", e)
+else:
+    print("O resultado da inversa é:", y)
