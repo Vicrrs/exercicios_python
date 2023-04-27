ee19bdd3f5bad997f5ffb40927414078499da915
Vicrrs <enricofermi2019@gmail.com>
2023-04-27 13:44:03 -0400

    ex05

---
+++ exceções/ex05.py
@@ -0,0 +1,8 @@
+try:
+    x = int(input("Digite um número: "))
+except ValueError:
+    print("Digite um número válido")
+else:
+    print("O número digitado foi: ", x)
+finally:
+    print("Fim do programa")
