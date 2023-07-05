aed3ca07625e9fd0c488a8abfc448ca9dd191e38
Vicrrs <enricofermi2019@gmail.com>
2023-07-05 10:15:49 -0400

    aula01 otavio mirando revisando

---
+++ aula_otavio_miranda/aula001.py
@@ -0,0 +1,20 @@
+"""
+Python é uma linguagem de programação de tipagem dinâmica e forte
+DocString
+Strings sao textos que estão dentro de aspas
+str -> string -> texto
+"""
+
+# Aspas simples
+print('Victor Roza')
+print('Victor "Roza"')
+
+# Aspas duplas
+print("Victor Roza")
+print("Victor 'Roza'")
+
+# Escape
+print("Victor \"Roza\"")
+
+# r - expressoes regulares
+print(r"Victor \"Roza\"")
