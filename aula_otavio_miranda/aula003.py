216aca13dff8074dc9d66553c4b9f89e28af9e78
Vicrrs <enricofermi2019@gmail.com>
2023-07-13 19:05:34 -0300

    booleano aula003

---
+++ aula_otavio_miranda/aula003.py
@@ -0,0 +1,10 @@
+# Tipo de dado bool (booleano)
+# Ao questinonar algo em um programa
+# só existem duas respostas possiveis
+# Sim (true) ou Não (False)
+# Existem vários operadores para "questionar"
+# Dentre eles o ==, que é um operador lógico que
+# questiona se um valor é igual ao outro
+print(10 == 10)
+print(10 == 11)
+print(type(10 == 11))
