2582191477395a3ea0fa5af1d6ab6be6df9c8502
Vicrrs <enricofermi2019@gmail.com>
2023-07-13 19:20:33 -0300

    aula004

---
+++ aula_otavio_miranda/aula004.py
@@ -0,0 +1,11 @@
+# conversão de tipos, corção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+# tipos imutaveis e primitivos
+# str, int, float, bool
+print("1", type("1"))
+print("1", type(int("1")))
+print(float("1") + 1)
+print(type(float("1") + 1))
+print(bool(" "))
+print(str(11) + "b")
