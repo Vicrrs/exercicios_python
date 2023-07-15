bd74658bd34a66f0aaac3d2631044d0fe843c632
Vicrrs <enricofermi2019@gmail.com>
2023-07-14 21:05:47 -0300

    exercicio

---
+++ aula_otavio_miranda/aula006.py
@@ -0,0 +1,13 @@
+nome = str(input("Digite seu nome: "))
+sobrenome = str(input("Digite seu sobrenome: "))
+idade = int(input("Digite sua idade: "))
+ano_nascimento = int(input("Digite seu ano de nascimento: "))
+maior_idade = idade >= 18
+altura = float(input("Digite sua altura: "))
+
+print("Nome: ", nome)
+print("Sobrenome: ", sobrenome)
+print("Idade: ", idade)
+print("Ano de nascimento: ", ano_nascimento)
+print("É maior de idade: ", maior_idade)
+print("Altura(m): ", altura)
