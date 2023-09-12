769f0e60fccc3fee3181e72c3bc77a93a430860b
rozas <enricofermi2019@gmail.com>
2023-09-12 02:50:06 -0300

    arvore binaria

--- Exercícios/ex013.py
+++ Exercícios/ex013.py
@@ -1,35 +1,30 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+# Arvores Binarias
+class No:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esquerda = None
+        self.direita = None
 
-# Parâmetros
-comprimento_onda = 2.0  # Comprimento de onda da onda (unidades arbitrárias)
-comprimento_fenda = 5.0  # Largura da fenda (unidades arbitrárias)
-tempo_total = 10.0  # Tempo total da simulação (s)
-dt = 0.1  # Intervalo de tempo (s)
-numero_pontos = 1000
+def inserir(raiz, valor):
+    if raiz is None:
+        return No(valor)
+    else:
+        if valor <= raiz.valor:
+            raiz.esquerda = inserir(raiz.esquerda, valor)
+        else:
+            raiz.direita = inserir(raiz.direita, valor)
+    return raiz
 
-# Inicialização
-tempo = np.arange(0, tempo_total, dt)
-posicao = np.linspace(-comprimento_fenda / 2, comprimento_fenda / 2, numero_pontos)
-amplitude = np.sin(2 * np.pi * posicao / comprimento_onda)  # Função de onda inicial
+def em_ordem(raiz):
+    if raiz:
+        em_ordem(raiz.esquerda)
+        print(raiz.valor, end=' ')
+        em_ordem(raiz.direita)
 
-# Função para atualizar a animação em cada quadro
-def atualizar(frame):
-    onda.set_data(posicao, amplitude * np.sin(2 * np.pi * (posicao - tempo[frame]) / comprimento_onda))
-    return onda,
-
-# Configuração do gráfico
-fig, ax = plt.subplots()
-ax.set_xlim(-comprimento_fenda / 2, comprimento_fenda / 2)
-ax.set_ylim(-1.2, 1.2)
-onda, = ax.plot([], [], 'b-', lw=2)
-ax.set_xlabel('Posição (unidades)')
-ax.set_ylabel('Amplitude')
-ax.set_title('Animação da Difração de uma Onda por uma Fenda')
-
-# Criação da animação
-ani = FuncAnimation(fig, atualizar, frames=len(tempo), blit=True)
-
-# Exibição da animação
-plt.show()
+raiz = None
+raiz = inserir(raiz, 50)
+raiz = inserir(raiz, 30)
+raiz = inserir(raiz, 70)
+raiz = inserir(raiz, 20)
+raiz = inserir(raiz, 40)
+em_ordem(raiz)  # Saída: 20 30 40 50 70
