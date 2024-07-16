d0771a7c56dabd9dc50557ac68c88431b5d9c1aa
Vicrrs <enricofermi2019@gmail.com>
2024-07-16 16:00:23 -0400

    Encapsulamento com Propriedades, adicionando teoria.

--- POO/revisa_POO/avancado/ex001.py
+++ POO/revisa_POO/avancado/ex001.py
@@ -3,6 +3,10 @@
 
 # Encapsulamento com Propriedades
 
+"""
+    Teoria: Utilizarmos propriedades para encapsular o acesso ao atributo `saldo`, permitindo validação
+    ao modificar o saldo.
+"""
 
 class ContaBancaria:
     def __init__(self, saldo):
