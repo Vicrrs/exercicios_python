afa07a1f74ca135c71b1e0b1371c86ba90707514
Vicrrs <enricofermi2019@gmail.com>
2024-07-16 15:59:02 -0400

    Encapsulamento com Propriedades

---
+++ POO/revisa_POO/avancado/ex001.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Encapsulamento com Propriedades
+
+
+class ContaBancaria:
+    def __init__(self, saldo):
+        self._saldo = saldo
+
+    @property
+    def saldo(self):
+        return self._saldo
+
+    @saldo.setter
+    def saldo(self, valor):
+        if valor < 0:
+            raise ValueError("Saldo não pode ser negativo")
+        self._saldo = valor
+
+conta = ContaBancaria(100)
+print(conta.saldo) # 100
+conta.saldo = 200
+print(conta.saldo) # 200
+
